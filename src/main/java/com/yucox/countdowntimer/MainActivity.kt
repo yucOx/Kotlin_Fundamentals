6ce1fc9c416b20b01ba6b44ceee14bf4fab0d575
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-05-01 17:16:46 +0300

    Add files via upload

---
+++ src/main/java/com/yucox/countdowntimer/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.yucox.countdowntimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.yucox.countdowntimer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        object : CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+                binding.textView.text = "Left : ${p0 / 1000}"
+            }
+
+            override fun onFinish() {
+                binding.textView.text = "Left : 0"
+            }
+        }.start()
+    }
+}
